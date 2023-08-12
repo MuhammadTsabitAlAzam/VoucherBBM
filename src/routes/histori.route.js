ac66f9cb12be000cd035164de18ed716f9145bcd
MuhammadTsabitAlAzam <73046372+MuhammadTsabitAlAzam@users.noreply.github.com>
2023-08-12 15:53:29 +0700

    Update histori.route.js

--- src/routes/histori.route.js
+++ src/routes/histori.route.js
@@ -2,13 +2,17 @@ import express from 'express';
 import { historiCreateScheme, historiUpdateScheme } from '../schemes/histori.scheme.js';
 import { handleValidate } from '../middlewares/handleValidate.js';
 import {  } from '../controllers/histori.controller.js';
-import { createHistori, deleteHistori, getHistori, getHistoris, updateHistori } from '../controllers/histori.controller.js';
+import { createHistori, deleteHistori, getHistori, getHistoris, updateHistori, getHistoriAmbil, getHistoriKembali } from '../controllers/histori.controller.js';
 
 const router = express.Router();
 
 router.get('/histori', getHistori);
 
 router.get('/histori/:id', getHistoris);
+
+router.get('/historiambil', getHistoriAmbil);
+
+router.get('/historikembali', getHistoriKembali);
 
 router.post('/histori', handleValidate(historiCreateScheme), createHistori);
 
