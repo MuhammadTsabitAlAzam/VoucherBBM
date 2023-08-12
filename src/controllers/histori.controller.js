e97f3872d4578bda08e164109341837e3e88dc08
MuhammadTsabitAlAzam <73046372+MuhammadTsabitAlAzam@users.noreply.github.com>
2023-08-12 13:55:38 +0700

    Update histori.controller.js

--- src/controllers/histori.controller.js
+++ src/controllers/histori.controller.js
@@ -88,10 +88,26 @@ export const updateHistori = async (req, res) => {
 
 
 export const deleteHistori = async (req, res) => {
-
   const { id } = req.params;
 
   try {
+    const [historiRows] = await pool.query('SELECT * FROM histori WHERE id=?', [id]);
+    if (historiRows.length <= 0) {
+      return res.status(404).json({ message: 'Data histori Tidak Ditemukan.' });
+    }
+
+    const histori = historiRows[0];
+    if (histori.jenis === 'Ambil') {
+      // Jika jenis adalah 'Ambil', cek voucher dengan tanggal dan data yang cocok
+      const [voucherRows] = await pool.query('SELECT * FROM voucher WHERE nama=? AND bagian=? AND tanggal_pengambilan=?', [histori.nama, histori.bagian, histori.tanggal]);
+      if (voucherRows.length > 0) {
+        const voucher = voucherRows[0];
+        if (voucher.tanggal_pengembalian === null) {
+          // Hapus voucher jika tanggal_pengembalian masih null
+          await pool.query('DELETE FROM voucher WHERE id=?', [voucher.id]);
+        }
+      }
+    }
 
     const [result] = await pool.query('DELETE FROM histori WHERE id=?', [id]);
 
@@ -100,7 +116,8 @@ export const deleteHistori = async (req, res) => {
       : res.json({ success: true });
 
   } catch (error) {
-    
+
     return res.status(500).json({ message: 'SOMETHING GOES WRONG.' });
   }
 };
+
